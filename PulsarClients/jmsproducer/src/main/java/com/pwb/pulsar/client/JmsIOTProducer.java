a8ae60767c258de8e7003661091f1dab09add8a0
patri <patri@DESKTOP-PCL3O07.lan>
2022-07-29 10:32:57 -0500

    Updated

--- PulsarClients/jmsproducer/src/main/java/com/pwb/pulsar/client/JmsIOTProducer.java
+++ PulsarClients/jmsproducer/src/main/java/com/pwb/pulsar/client/JmsIOTProducer.java
@@ -1,10 +1,35 @@
 package com.pwb.pulsar.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+
+import com.datastax.oss.pulsar.jms.PulsarConnectionFactory;
+
 public class JmsIOTProducer {
+	private static final String YOUR_PULSAR_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTgyNjQ4MTQsImlzcyI6ImRhdGFzdGF4Iiwic3ViIjoiY2xpZW50OzRhYmY1MzA4LWE1MTUtNGZhOS1iYWVjLTk0ZDE5M2U2ZjA5YztZMlJqYzNSeVpXRnRjdz09OzE0ZTg1YzY5MDkiLCJ0b2tlbmlkIjoiMTRlODVjNjkwOSJ9.I4B_a-_3_HBzsg_QDbbpzg2GJKOvYbh019i__zgIY32hbmyRuG6edGQG2-ovyO9VFZDdsF8pR-PvIsTT-WqdmxQwNLINDX8BAjanEPvRbIs60cHr7P_rLseQYNqCAFj8CXD70T8xG-tzl8a5tNjN_rG4gZUBwKHDanL1HDUWPX0BTgk1gjcglQ66yxohHnbM4mvWmspiOONswWvki7kd0ixsTS51iA60KVh8tB4lIPSVX_k3blwbWezWBroibTMjnzdU2vySHvdIRcJMueBNsu4K5ixOQdevXymvU0X4Xh8HursWSuyVoAnMwxawJN-SnShPH2ipa3a9vxRpfFWOwg";
+    private static final String SERVICE_URL = "pulsar+ssl://pulsar-gcp-useast1.streaming.datastax.com:6651";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		  
+		Map<String, Object> configuration = new HashMap<>();
+		configuration.put("webServiceUrl", "https://pulsar-gcp-useast1.api.streaming.datastax.com");
+		configuration.put("brokerServiceUrl", "pulsar+ssl://pulsar-gcp-useast1.streaming.datastax.com:6651");
+        configuration.put("authPlugin", "org.apache.pulsar.client.impl.auth.AuthenticationToken");
+	    configuration.put("authParams", YOUR_PULSAR_TOKEN);
 
+		PulsarConnectionFactory factory = new PulsarConnectionFactory(configuration);
+		JMSContext context = factory.createContext();
+		Destination destination = context.createQueue("persistent://cdcstreams/cdc-iot/devicestatus");
+
+		Message msg = context.createTextMessage("ID045|temp=89|battery=99|location=45.232");
+        context.createProducer().send(destination, msg);
+        System.out.printf("Producer message sent\n");
+
+        context.close();
 	}
 
 }
